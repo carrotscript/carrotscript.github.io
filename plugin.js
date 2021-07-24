e8e67e22a320db1ca5f280dd8475ccc4ff4d5216
greatlygravitate <87620301+greatlygravitate@users.noreply.github.com>
2021-07-24 14:45:35 -0400

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -4,7 +4,7 @@
 /*
   <script src="https://carrotscript.github.io/plugin.js"></script>
 */
-// Create another script tag at your body, and type your code in there.
+// Create another script tag at your body, and type your code in there. (JavaSript will still work in the script tag)
 // Carrot On!
 
 var post;
@@ -12,6 +12,31 @@ function post(name, data) {
   post = name + "=" + data;
   document.cookie = post;
 }
-function get(name, varname) {
-  
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function print(name) {
+  var print = getCookie(name);
+  var btn = document.createElement("p");
+  btn.innerHTML = print;
+  document.body.appendChild(btn);
+}
+
+function replace(id, cookie) {
+  var change = getCookie(cookie);
+  doument.getElementById(id).innerHTML = change;
 }
