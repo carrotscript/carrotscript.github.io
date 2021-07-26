a564ebe7c83f31fc9ab333e847dec0f37bb048c7
greatlygravitate <87620301+greatlygravitate@users.noreply.github.com>
2021-07-26 17:15:37 -0400

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -7,16 +7,22 @@
 // Create another script tag at your body, and type your code in there. (JavaSript will still work in the script tag)
 // Carrot On!
 
-
-console.warn("The CarrotScript Plugin has been added to this website. \n The website agrees to the Terms and Conditions \n Visit the Terms and Conditions at https://carrotscript.github.io/terms.");
+console.group("CarrotScript Messages");
+console.warn("The CarrotScript Plugin has been added to this website. \n The owner agrees to the Terms and Conditions \n Visit the Terms and Conditions at https://carrotscript.github.io/terms.");
 console.log("CarrotScript started...");
 var post;
 function post(name, data) {
-  post = name + "=" + data;
-  document.cookie = post;
+  try {
+    post = name + "=" + data;
+    document.cookie = post;
+  }
+  catch(err) {
+   console.error("post(); error: " + err);
+  }
 }
 
 function get(cname) {
+  try {
   let name = cname + "=";
   let decodedCookie = decodeURIComponent(document.cookie);
   let ca = decodedCookie.split(';');
@@ -30,6 +36,10 @@ function get(cname) {
     }
   }
   return "";
+  }
+  catch(err) {
+   console.error("get(); error: " + err);
+  }
 }
 
 function print(name) {
@@ -37,11 +47,18 @@ function print(name) {
   var btn = document.createElement("p");
   btn.innerHTML = print;
   document.body.appendChild(btn);
+  catch(err) {
+   console.error("print(); error: " + err);
+  }
 }
 
 function replace(id, cookie) {
   var change = get(cookie);
   document.getElementById(id).innerHTML = change;
+  catch(err) {
+   console.error("replace(); error: " + err);
+  }
 }
 
 console.log("CarrotScript finished...");
+console.groupEnd();
