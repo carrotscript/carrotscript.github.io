2fa1820b1558ac343541be6d7a55bd1c32cfd386
greatlygravitate <87620301+greatlygravitate@users.noreply.github.com>
2021-08-11 14:22:45 -0400

    Update files.js

--- files.js
+++ files.js
@@ -1,31 +1,10 @@
-// You may be wondering what this is about. Well, this generates files for our file generator.
-
-    function dynamic_text() {
-        return document.getElementById("data").value;
+  function SaveAsFile(t,f,m) {
+    try {
+      var b = new Blob([t],{type:m});
+      saveAs(b, f);
+    } catch (e) {
+      window.open("data:"+m+"," + encodeURIComponent(t), '_blank','');
     }
+  }
 
-
-function download_file(name, contents, mime_type) {
-
-    
-        mime_type = mime_type || "text/plain";
-
-        var blob = new Blob([contents], {type: mime_type});
-
-        var dlink = document.createElement('a');
-        dlink.download = name;
-        dlink.href = window.URL.createObjectURL(blob);
-        dlink.onclick = function(e) {
-            // revokeObjectURL needs a delay to work properly
-            var that = this;
-            setTimeout(function() {
-                window.URL.revokeObjectURL(that.href);
-            }, 1500);
-        };
-        console.log(dlink.href);
-        dlink.click();
-        dlink.remove();
-    }
-
-
-// window.location.href = "https://carrotscript.github.io/open_file";
+SaveAsFile("text","project.carrotscript","text/plain;charset=utf-8");
