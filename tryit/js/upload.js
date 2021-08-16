f56df4f60506007d63fc52760f4dff1c573c0406
greatlygravitate <87620301+greatlygravitate@users.noreply.github.com>
2021-08-16 14:24:52 -0400

    Update and rename change.js to upload.js

--- tryit/js/upload.js
+++ tryit/js/upload.js
@@ -1,14 +1,14 @@
-function init() {
+function init(){
   document.getElementById('fileInput').addEventListener('change', handleFileSelect, false);
 }
 
-function handleFileSelect(event) {
+function handleFileSelect(event){
   const reader = new FileReader()
   reader.onload = handleFileLoad;
   reader.readAsText(event.target.files[0])
 }
 
-function handleFileLoad(event) {
+function handleFileLoad(event){
   console.log(event);
   // document.getElementById('fileContent').textContent = event.target.result;
 }
