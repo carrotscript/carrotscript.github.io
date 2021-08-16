aafb86eb3d7a1cb81453249c574a9caa015bda33
greatlygravitate <87620301+greatlygravitate@users.noreply.github.com>
2021-08-16 14:51:42 -0400

    Update change.js

--- tryit/js/change.js
+++ tryit/js/change.js
@@ -1,4 +1,9 @@
 const input = document.querySelector('input[type="file"')
 input.addEventListener('change', function (e) {
   console.log(input.files)
+  const reader = new FileReader()
+  reader.onload = function() {
+    console.log(reader.result)
+  }
+  reader.readAsTest(input.files[0])
 }, false)
