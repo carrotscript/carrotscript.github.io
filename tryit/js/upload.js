3ff3efb3aa2e4543e043f97b0a657fe0f3251247
greatlygravitate <87620301+greatlygravitate@users.noreply.github.com>
2021-08-16 15:32:54 -0400

    Update and rename read.js to upload.js

--- tryit/js/upload.js
+++ tryit/js/upload.js
@@ -1,13 +1,18 @@
 const input = document.querySelector('input[type="file"]')
+var data
 input.addEventListener('change', function (e) {
   console.log(input.files)
   const reader = new FileReader()
   reader.onload = function() {
     console.log(reader.result)
-    var data = reader.result;
-    var script = document.createElement("script");
-    script.innerHTML = data;
-    document.body.appendChild(script);
+    data = reader.result;
+    
+   }
   }
   reader.readAsText(input.files[0])
 }, false)
+    console.log(data);
+    var script = document.createElement('script');
+    script.innerHTML = data;
+    console.log(script);
+    document.body.appendChild(script);
