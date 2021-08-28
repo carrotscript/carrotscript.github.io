5cddf4c4f242bacf9ff86311d458706aa6e9e8db
greatlygravitate <87620301+greatlygravitate@users.noreply.github.com>
2021-08-28 19:53:47 -0400

    Create terminal.sh

---
+++ terminal/terminal.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Downloading Terminal..."
+echo ""
+wget https://carrotscript.github.io/terminal/carrotscriptterminal.py
+echo ""
+echo "Downloading Terminal Interface"
+wget https://carrotscript.github.io/terminal/download.py
+echo ""
+echo "Creating Terminal..."
+python3 download.py
+echo "Opening terminal... (Run carrotscript to pen terminal)"
+carrotscript
+source .bashrc
