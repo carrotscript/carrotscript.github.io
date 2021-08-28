b3f43e89c94dbefc3442eb1f3076fe3d361c3d73
greatlygravitate <87620301+greatlygravitate@users.noreply.github.com>
2021-08-28 15:13:47 -0400

    Create download.py

---
+++ terminal/download.py
@@ -0,0 +1,19 @@
+# This file creates the CarrotScript command line interface
+
+import os
+
+
+commands = open("carrotscript.py", "w")
+commands.write("print('It works')")
+commands.close()
+
+alias = 'alias carrotscript="echo hello world"\n'
+homefolder = os.path.expanduser('~')
+bashrc = os.path.abspath('%s/.bashrc' % homefolder)
+
+with open(bashrc, 'r') as f:
+  lines = f.readlines()
+  if alias not in lines:
+    out = open(bashrc, 'a')
+    out.write(alias)
+    out.close()
