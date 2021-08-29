28fcb23284ca4f550bd156f357d348309fbec13a
greatlygravitate <87620301+greatlygravitate@users.noreply.github.com>
2021-08-29 18:04:53 -0400

    Update terminal.sh

--- terminal/linux/terminal.sh
+++ terminal/linux/terminal.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 echo "Welcome to the CarrotScript Terminal vL.1.0. This is for Linux versions."
-sleep 1
+echo "In 3 seconds, you should be given the downloadin process."
+sleep 3
 echo "Downloading Terminal..."
 echo ""
 wget https://carrotscript.github.io/terminal/linux/carrotscriptterminal.py
+sleep 1
 echo ""
 echo "Downloading Terminal Interface"
 wget https://carrotscript.github.io/terminal/linux/download.py
+sleep 1
 echo ""
 echo "Creating Terminal..."
 python3 download.py
+sleep 1
 source .bashrc
+sleep 1
 echo "Removing non-nessasary files..."
 rm download.py
+sleep 1
 echo ""
 echo ""
 echo ""
@@ -20,5 +26,5 @@ echo ""
 echo ""
 echo ""
 echo "Opening terminal... (Run carrotscript to open terminal)"
-echo "Status: 200 OK"
+sleep 1
 python3 carrotscriptterminal.py
