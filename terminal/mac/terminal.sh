c6df92c51f4cd547f72f97346edd106535c123ea
greatlygravitate <87620301+greatlygravitate@users.noreply.github.com>
2021-08-29 11:36:21 -0400

    Create terminal.sh

---
+++ terminal/mac/terminal.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo "Downloading Terminal..."
+echo ""
+wget https://carrotscript.github.io/terminal/linux/carrotscriptterminal.py
+echo ""
+echo "Downloading Terminal Interface"
+wget https://carrotscript.github.io/terminal/mac/download.py
+echo ""
+echo "Creating Terminal..."
+python3 download.py
+source .bashrc
+echo "Removing non-nessasary files..."
+rm download.py
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo "Opening terminal... (Run carrotscript to open terminal)"
+echo "Status: 200 OK"
+python3 carrotscriptterminal.py
