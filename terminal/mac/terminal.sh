06730fe1dcf611222385a40f780bb209e4e9fbc6
greatlygravitate <87620301+greatlygravitate@users.noreply.github.com>
2021-08-29 18:01:29 -0400

    Update terminal.sh

--- terminal/mac/terminal.sh
+++ terminal/mac/terminal.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo "Welcome to the CarrotScript Terminal vL.1.0. This is for Linux versions."
+sleep 1
 echo "Downloading Terminal..."
 echo ""
 wget https://carrotscript.github.io/terminal/linux/carrotscriptterminal.py
