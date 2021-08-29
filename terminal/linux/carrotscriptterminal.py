a88b2ccf2d498d64cb61fc8af69ffaa078b559da
greatlygravitate <87620301+greatlygravitate@users.noreply.github.com>
2021-08-29 16:14:40 -0400

    Update carrotscriptterminal.py

--- terminal/linux/carrotscriptterminal.py
+++ terminal/linux/carrotscriptterminal.py
@@ -1,13 +1,14 @@
 import sys, os
 
-print("Welcome to the CarrotScript terminal.")
+print("Welcome to the CarrotScript vBETA.1 terminal.")
 print("This terminal is experimental. Some features may not work.")
 print('Type "help" for a list of commands')
 command = input(">>> ")
 
 if command == "help":
     print("Commands:")
-    print("localhost")
+    print("localhost [Start a localhost:XXXX test development server]")
+    print("exit() [Exit current session]")
     command = input(">>> ")
     
 if command == "localhost":
