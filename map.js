29877ed60310d2ca407d3e00f479dce3096d28de
greatlygravitate <87620301+greatlygravitate@users.noreply.github.com>
2021-10-04 18:35:31 -0400

    Update map.js

--- map.js
+++ map.js
@@ -7,8 +7,7 @@ const mapjs = {
         googlemap.innerHTML = '<iframe width="' + width + '" height="' + heigth + '" style="border:0" loading="lazy" allowfullscreen src="https://www.google.com/maps/embed/v1/place?key=AIzaSyC7HAr3ww-RvJii8vmOMTU6RNV8O0AQ8TE&q=' + place + '"></iframe>';
         document.body.appendChild(googlemap);
     },
-    waze: function(lat, long, width, heigth, pin) {
-    	if (typeof pin === 'undefined') { pin = '0'; }
+    waze: function(lat, long, width, heigth) {
     	if (typeof width === 'undefined') { width = '600'; }
 		if (typeof heigth === 'undefined') { heigth = '450'; }
     	var wazemap = document.createElement("div");
